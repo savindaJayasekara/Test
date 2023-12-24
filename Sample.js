2500ba64b8390aa00f21b7de53bb818735cb90b8
savinda <148347943+savindaJayasekara@users.noreply.github.com>
2023-12-24 20:27:00 +0530

    Update Sample.js

--- Sample.js
+++ Sample.js
@@ -1,1 +1,13 @@
-console.log('Hello World !')
+const express = requre('express');
+const controller = require('./controller/controller')
+const app = express();
+
+app.use(express.json());
+
+console.log('Hello World !');
+
+app.put('/deleteuser',(req,res)=>{
+    controller.deleteUser(req,res);
+});
+
+module.exports = app;
